61312991fcb463bbedef97f5bd33137b2cfc6ed0
soju6jan <cybersol@naver.com>
2020-06-06 11:18:49 +0900

    20200606-1118

--- epg_commit.sh
+++ epg_commit.sh
@@ -2,7 +2,12 @@
 export LANG=en_US.utf8
 NowDate=$(date +%Y%m%d)-$(date +%H%M)
 APP_HOME=/home/coder/project/SJ/SJVA2
-cp $APP_HOME/etc/epg.db $APP_HOME/data/sjva_support
+if [ -f "$APP_HOME/data/db/epg.db" ] ; then
+    cp $APP_HOME/data/db/epg.db $APP_HOME/data/sjva_support
+fi
+if [ -f "$APP_HOME/data/output/xmltv_all.xml" ] ; then
+    cp $APP_HOME/data/output/xmltv_all.xml $APP_HOME/data/sjva_support
+fi
 cd $APP_HOME/data/sjva_support && 
 git --git-dir $APP_HOME/data/sjva_support/.git add * && 
 git --git-dir $APP_HOME/data/sjva_support/.git commit -m $NowDate && 
