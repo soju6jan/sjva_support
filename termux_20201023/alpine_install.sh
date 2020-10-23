a26b0314971d16cf33e9d6cd44ec8ab2c7f88ca0
soju6jan <cybersol@naver.com>
2020-10-23 18:57:48 +0900

    1

--- termux_20201023/alpine_install.sh
+++ termux_20201023/alpine_install.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
+cd $HOME
+mv alpine_profile2 /root/.profile
 apk update
 apk add --no-cache git ffmpeg
-apk add --no-cache python3 py3-pip python3-dev py3-lxml py3-sqlalchemy py3-google-api-python-client py3-pillow py3-gevent py3-gevent-websocket py3-yarl py3-aiohttp py3-pycryptodome py3-google-api-python-client
+apk add --no-cache python3 py3-pip python3-dev py3-lxml py3-sqlalchemy py3-google-api-python-client py3-pillow py3-gevent py3-gevent-websocket py3-yarl py3-aiohttp py3-pycryptodome py3-markupsafe py3-wcwidth
+#py3-google-api-python-client
 
 pip3 install flask-login flask-socketio flask-sqlalchemy pytz apscheduler markdown selenium celery redis telepot sqlitedict 
 guessit plexapi
@@ -9,7 +12,4 @@ guessit plexapi
 cd ~
 git clone --depth 1 https://github.com/soju6jan/sjva2_src_obfuscate SJVA2
 
-curl -LO https://raw.githubusercontent.com/soju6jan/sjva_support/master/termux_20201023/alpine_profile2
-mv alpine_profile2 /root/.profile
 sh /root/.profile
-
--- termux_20201023/sjva_install.sh
+++ termux_20201023/sjva_install.sh
@@ -2,13 +2,15 @@
 git='https://raw.githubusercontent.com/soju6jan/sjva_support/master/termux_20201023'
 alpine='/data/data/com.termux/files/usr/share/TermuxAlpine'
 
-curl -LO $git/TermuxAlpine_sjva.sh &&
-bash TermuxAlpine_sjva.sh &&
-curl -LO $git/alpine_profile1 &&
-mv alpine_profile1 $alpine/root/.profile &&
-curl -LO $git/alpine_install.sh &&
-mv alpine_install.sh $alpine/home/alpine_install.sh &&
-cd $HOME &&
-curl -LO $git/termux_bash_profile &&
-mv termux_bash_profile ~/.profile &&
+curl -LO $git/TermuxAlpine_sjva.sh
+bash TermuxAlpine_sjva.sh
+curl -LO $git/alpine_profile1
+mv alpine_profile1 $alpine/root/.profile
+curl -LO $git/alpine_profile2
+mv alpine_profile2 $alpine/home/alpine_profile2
+curl -LO $git/alpine_install.sh
+mv alpine_install.sh $alpine/home/alpine_install.sh
+cd $HOME
+curl -LO $git/termux_bash_profile
+mv termux_bash_profile ~/.profile
 startalpine
