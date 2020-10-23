fe6cf8228a7872720c1db6961f63034c99dcce95
soju6jan <cybersol@naver.com>
2020-10-23 21:07:12 +0900

    1

--- termux_20201023/alpine_install.sh
+++ termux_20201023/alpine_install.sh
@@ -1,16 +1,24 @@
 #!/bin/sh
+echo "============================"
+echo $@
 cd /home
 mv /home/alpine_profile2 /root/.profile
 apk update
 apk add --no-cache git 
 apk add --no-cache python3 py3-pip python3-dev py3-lxml py3-sqlalchemy py3-gevent py3-gevent-websocket py3-pycryptodome py3-markupsafe 
-# ffmpeg py3-pillow py3-yarl py3-aiohttp py3-wcwidth py3-google-api-python-client
 pip3 install flask-login flask-socketio flask-sqlalchemy pytz apscheduler markdown selenium celery redis telepot sqlitedict 
-#pip install guessit plexapi
+if [ $1 == "full" ]; then
+apk add --no-cache ffmpeg py3-pillow py3-yarl py3-aiohttp py3-wcwidth py3-google-api-python-client
+pip install guessit plexapi
+fi
 
 cd /home
 git clone --depth 1 https://github.com/soju6jan/sjva2_src_obfuscate SJVA2
 mkdir -p /home/SJVA2/bin/LinuxArm
-mv /home/filebrowser /home/SJVA2/bin/LinuxArm
-mv /home/rclone /home/SJVA2/bin/LinuxArm
+if [ -f "/home/filebrowser" ] ; then
+  mv /home/filebrowser /home/SJVA2/bin/LinuxArm
+fi
+if [ -f "/home/rclone" ] ; then
+  mv /home/rclone /home/SJVA2/bin/LinuxArm
+fi
 cd SJVA2 && ./start_termux.sh
--- termux_20201023/sjva_install.sh
+++ termux_20201023/sjva_install.sh
@@ -2,15 +2,10 @@
 git='https://raw.githubusercontent.com/soju6jan/sjva_support/master/termux_20201023'
 git2='https://raw.githubusercontent.com/soju6jan/SJVA2/master/bin/LinuxArm'
 alpine='/data/data/com.termux/files/usr/share/TermuxAlpine'
-
-echo "#################"
-echo $@
-echo "#################"
-
 curl -LO $git/TermuxAlpine_sjva.sh
-#bash TermuxAlpine_sjva.sh
+bash TermuxAlpine_sjva.sh
 curl -LO $git/alpine_profile1
-sed -i 's/args/$@/' alpine_profile1
+sed -i "s/args/$@/" alpine_profile1
 cat alpine_profile1
 exit
 mv alpine_profile1 $alpine/root/.profile
@@ -18,10 +13,12 @@ curl -LO $git/alpine_profile2
 mv alpine_profile2 $alpine/home
 curl -LO $git/alpine_install.sh
 mv alpine_install.sh $alpine/home/alpine_install.sh
+if [ $1 == "full" ]; then
 curl -LO $git2/filebrowser
 mv filebrowser $alpine/home
 curl -LO $git2/rclone
 mv rclone $alpine/home
+fi
 cd $HOME
 curl -LO $git/termux_bash_profile
 mv termux_bash_profile ~/.profile
