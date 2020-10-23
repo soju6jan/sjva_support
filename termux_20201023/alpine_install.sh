f6f53c3db1af23036272497905aba3abfff018c6
soju6jan <cybersol@naver.com>
2020-10-23 18:46:53 +0900

    1

--- termux_20201023/alpine_install.sh
+++ termux_20201023/alpine_install.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 apk update
-apk add --no-cache git python3 py3-pip python3-dev py3-lxml py3-sqlalchemy py3-google-api-python-client py3-pillow apk py3-gevent py3-gevent-websocket py3-yarl py3-aiohttp py3-pycryptodome py3-google-api-python-client ffmpeg
+apk add --no-cache git ffmpeg
+apk add --no-cache python3 py3-pip python3-dev py3-lxml py3-sqlalchemy py3-google-api-python-client py3-pillow py3-gevent py3-gevent-websocket py3-yarl py3-aiohttp py3-pycryptodome py3-google-api-python-client
 
 pip3 install flask-login flask-socketio flask-sqlalchemy pytz apscheduler markdown selenium celery redis telepot sqlitedict 
 guessit plexapi
