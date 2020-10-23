0f07df79e674cec7fc47e21585fa63b30c31bd86
soju6jan <cybersol@naver.com>
2020-10-23 18:33:59 +0900

    1

--- termux_20201023/sjva_install.sh
+++ termux_20201023/sjva_install.sh
@@ -1,24 +1,14 @@
 #!/bin/bash
-curl -LO https://raw.githubusercontent.com/soju6jan/sjva_support/master/termux_20201023/TermuxAlpine_sjva.sh 
-bash TermuxAlpine_sjva.sh
-rm TermuxAlpine_sjva.sh
+git='https://raw.githubusercontent.com/soju6jan/sjva_support/master/termux_20201023'
+alpine='/data/data/com.termux/files/usr/share/TermuxAlpine'
 
-
-cd $HOME
-echo "1. Download script 1/3"
-curl -LO https://raw.githubusercontent.com/soju6jan/sjva_support/master/termux_20201023/alpine_profile1
-mv alpine_profile1 /data/data/com.termux/files/usr/share/TermuxAlpine/root/.profile
-
-echo "1. Download script 2/3"
-curl -LO https://raw.githubusercontent.com/soju6jan/sjva_support/master/termux_20201023/alpine_install.sh
-mv alpine_install.sh /data/data/com.termux/files/usr/share/TermuxAlpine/home/alpine_install.sh
-
-echo "1. Download script 3/3"
-cd $HOME
-curl -LO https://raw.githubusercontent.com/soju6jan/sjva_support/master/termux_20201023/termux_bash_profile
-mv termux_bash_profile ~/.profile
-
-
-echo "2. Install Alpine.."
-
+curl -LO $git/TermuxAlpine_sjva.sh | bash &&
+rm TermuxAlpine_sjva.sh &&
+curl -LO $git/alpine_profile1 &&
+mv alpine_profile1 $alpine/root/.profile &&
+curl -LO $git/alpine_install.sh &&
+mv alpine_install.sh $alpine/home/alpine_install.sh &&
+cd $HOME &&
+curl -LO $git/termux_bash_profile &&
+mv termux_bash_profile ~/.profile &&
 startalpine
