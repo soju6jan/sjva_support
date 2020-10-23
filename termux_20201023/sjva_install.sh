7f16981de263e7794a4ac88e9f18d24ddf2546d8
soju6jan <cybersol@naver.com>
2020-10-23 20:56:03 +0900

    1

--- termux_20201023/sjva_install.sh
+++ termux_20201023/sjva_install.sh
@@ -10,7 +10,7 @@ echo "#################"
 curl -LO $git/TermuxAlpine_sjva.sh
 #bash TermuxAlpine_sjva.sh
 curl -LO $git/alpine_profile1
-sed -i 's/#args/$@/' alpine_profile1
+sed -i 's/args/$@/' alpine_profile1
 cat alpine_profile1
 exit
 mv alpine_profile1 $alpine/root/.profile
