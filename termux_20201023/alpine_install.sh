40b7671afb453b5ee28f2e4ae9272f8e15f6bd71
soju6jan <cybersol@naver.com>
2020-10-23 23:32:10 +0900

    1

--- termux_20201023/alpine_install.sh
+++ termux_20201023/alpine_install.sh
@@ -7,11 +7,10 @@ apk update
 apk add --no-cache git 
 apk add --no-cache python3 py3-pip python3-dev py3-lxml py3-sqlalchemy py3-gevent py3-gevent-websocket py3-pycryptodome py3-markupsafe py3-yarl py3-aiohttp 
 pip3 install flask-login flask-socketio flask-sqlalchemy pytz apscheduler markdown selenium celery redis telepot sqlitedict 
-if [ $1 == "full" ]; then
+if [ "$1" == "full" ]; then
 apk add --no-cache ffmpeg py3-pillow py3-wcwidth py3-google-api-python-client
 pip install guessit plexapi
 fi
-
 cd /home
 git clone --depth 1 https://github.com/soju6jan/sjva2_src_obfuscate SJVA2
 mkdir -p /home/SJVA2/bin/LinuxArm
