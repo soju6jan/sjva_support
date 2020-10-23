6ae2bd004d63baa740f897aab244d98b6a71b9e4
soju6jan <cybersol@naver.com>
2020-10-23 18:15:38 +0900

    1

--- termux_20201023/sjva_install.sh
+++ termux_20201023/sjva_install.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 cd $HOME
 echo "1. Install Alpine.."
-curl -LO https://raw.githubusercontent.com/soju6jan/sjva_support/master/termux_20201023/TermuxAlpine_sjva.sh
+
+
+
+curl https://raw.githubusercontent.com/soju6jan/sjva_support/master/termux_20201023/TermuxAlpine_sjva.sh | bash
+
 
 exit
 
@@ -12,12 +16,15 @@ curl -LO https://soju6jan.com/file/TermuxAlpine32.tar.gz
 tar -zxvf TermuxAlpine32.tar.gz
 rm TermuxAlpine32.tar.gz
 mv TermuxAlpine/startalpine ../bin
+
+
 chmod 777 ../bin/startalpine
-curl -LO https://raw.githubusercontent.com/soju6jan/SJVA2/master/etc/termux/alpine_profile1
+curl -LO https://raw.githubusercontent.com/soju6jan/sjva_support/master/termux_20201023/alpine_profile1
 mv alpine_profile1 /data/data/com.termux/files/usr/share/TermuxAlpine/root/.profile
-curl -LO https://raw.githubusercontent.com/soju6jan/SJVA2/master/etc/termux/alpine_install.sh
+
+curl -LO https://raw.githubusercontent.com/soju6jan/sjva_support/master/termux_20201023/alpine_install.sh
 mv alpine_install.sh /data/data/com.termux/files/usr/share/TermuxAlpine/home/alpine_install.sh
 cd $HOME
-curl -LO https://raw.githubusercontent.com/soju6jan/SJVA2/master/etc/termux/termux_bash_profile
+curl -LO https://raw.githubusercontent.com/soju6jan/sjva_support/master/termux_20201023/termux_bash_profile
 mv termux_bash_profile ~/.profile
 startalpine
