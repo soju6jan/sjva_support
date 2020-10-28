603fd9181ceb7f992860f0a997ded828b8575d10
soju6jan <cybersol@naver.com>
2020-10-28 11:41:56 +0900

    20201028-1141

--- termux_20201023/alpine_install.sh
+++ termux_20201023/alpine_install.sh
@@ -5,7 +5,7 @@ cd /home
 mv /home/alpine_profile2 /root/.profile
 apk update
 apk add --no-cache git 
-apk add --no-cache python3 py3-pip python3-dev py3-lxml py3-sqlalchemy py3-gevent py3-gevent-websocket py3-pycryptodome py3-markupsafe py3-yarl py3-aiohttp py3-wheel py3-markdown
+apk add --no-cache python3 py3-pip python3-dev py3-lxml py3-sqlalchemy py3-gevent py3-gevent-websocket py3-pycryptodome py3-markupsafe py3-yarl py3-aiohttp py3-wheel py3-markdown py3-psutil
 pip3 install flask-login flask-socketio flask-sqlalchemy pytz apscheduler selenium celery redis telepot sqlitedict 
 if [ "$1" == "full" ]; then
 apk add --no-cache ffmpeg py3-pillow py3-wcwidth py3-google-api-python-client
