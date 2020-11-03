eaadb9744bacfd894782dc974f234dab569b03cb
soju6jan <cybersol@naver.com>
2020-11-03 14:07:39 +0900

    1

--- termux/si.sh
+++ termux/si.sh
@@ -1,24 +1,23 @@
 #!/data/data/com.termux/files/usr/bin/bash
 #LINE="========================================================"
-mv $0 $PREFIX/bin/si
-chmod +x $PREFIX/bin/si
+if [ $0 != $PREFIX/bin/si ]; then
+    mv -f $0 $PREFIX/bin/si
+    chmod +x $PREFIX/bin/si
+fi
 LINE="==========================================="
 SJVA_HOME=$HOME/sjva
 DIR_DATA=$HOME/storage/downloads/sjva
-DIR_BIN=$HOME/bin/LinuxArm
+DIR_BIN=$SJVA_HOME/bin/LinuxArm
 GIT1="https://github.com/soju6jan/sjva2_src_obfuscate"
 GIT2="https://raw.githubusercontent.com/soju6jan/SJVA2"
-PACKAGE_CMD="apt"
+PACKAGE_CMD="apt -y"
 WHOAMI=`whoami`
 
 menu() {
     clear
+    echo -e " SJVA 설치 스크립트 v1.0"
     echo $LINE
-    echo -e " SJVA 설치 스크립트 v0.1"
-    echo $LINE
-    echo -e "<Debug>"
-    echo "0. SJVA foreground 실행"
-    echo -e "\n<Install>"
+    echo -e "<Install>"
     echo "1. 저장소 접근 권한 허용"
     echo "2. SJVA 설치 (최소)"
     echo "3. 파일브라우저(SJVA용) 설치"
@@ -26,20 +25,23 @@ menu() {
     echo "5. ffmpeg 설치" 
     echo "6. nginx 설치"
     echo "9. SJVA 설치 (전체)"
-    echo -e "\nTool"
-    echo "a. aria2c 설치"
-    echo "b. rclone 설치"    
-    echo "c. transmission 설치"
-    echo "d. sshd 설치"
-    echo "e. code-server 설치"    
+    echo -e "\n<Tool>"
+    echo "a. aria2 설치"
+    echo "b. rclone 설치" 
+    echo "c. sshd 설치"   
+    echo "d. transmission 설치"
+    echo "e. code-server 설치"
+    echo "f. code-server 암호변경"     
+    echo "y. kill all process" 
+    echo "z. SJVA foreground 실행"
     echo $LINE
 }
 
 base() {
     echo -e "\n\nSJVA 설치합니다."
-    $PACKAGE_CMD -y update
-    #$PACKAGE_CMD -y upgrade 
-    $PACKAGE_CMD -y install termux-services git wget python libxml2 libxslt 
+    $PACKAGE_CMD update
+    #$PACKAGE_CMD upgrade 
+    $PACKAGE_CMD install termux-services git wget python libxml2 libxslt 
     python3 -m pip install --upgrade pip
     pip3 install --upgrade setuptools
     pip3 install wheel
@@ -57,16 +59,14 @@ base() {
     fi
     #service 
     mkdir -p $PREFIX/var/service/sjva
-    cat <<EOF >$PREFIX/var/service/sjva/run
-#!/data/data/com.termux/files/usr/bin/sh
-cd $HOME/sjva
-bash ./s.sh
+    cat <<- EOF >$PREFIX/var/service/sjva/run
+    #!/data/data/com.termux/files/usr/bin/sh
+    cd $HOME/sjva
+    bash ./start_termux_native.sh
 EOF
     chmod +x $PREFIX/var/service/sjva/run
-    sv-enable sjva
+    sv-enable sjva > /dev/null 2>&1
 }
-
-
 
 install_filebrowser() {
     echo -e "\n\n파일브라우저(SJVA용)를 설치합니다."
@@ -88,12 +88,12 @@ install_rclone() {
 
 install_ffmpeg() {
     echo -e "\n\nffmpeg 설치를 시작합니다."
-    $PACKAGE_CMD -y install --upgrade ffmpeg
+    $PACKAGE_CMD install --upgrade ffmpeg
 }
 
 install_nginx() {
     echo -e "\n\nnginx 설치를 시작합니다."
-    $PACKAGE_CMD -y install --upgrade nginx php-fpm sqlite
+    $PACKAGE_CMD install --upgrade nginx php-fpm sqlite
     mkdir -p $SJVA_HOME/data/custom
     rm -rf $SJVA_HOME/data/custom/nginx
     git clone --depth 1 https://github.com/soju6jan/nginx $SJVA_HOME/data/custom/nginx
@@ -114,25 +114,36 @@ install_nginx() {
     cp -f $SJVA_HOME/data/custom/nginx/files/index.html $DIR_DATA/nginx/www/index.html
     cp -f $SJVA_HOME/data/custom/nginx/files/phpinfo.php $DIR_DATA/nginx/www/phpinfo.php
     cp -f $SJVA_HOME/data/custom/nginx/files/guide.php $DIR_DATA/nginx/www/guide.php
-    sv-enable nginx
-    sv-enable php-fpm
+    sv-enable nginx > /dev/null 2>&1
+    sv-enable php-fpm > /dev/null 2>&1
 }
 
 install_code_server() {
     echo -e "\n\ncode-server를 설치합니다.\n"
-    read -r -p "사용할 암호를 입력하세요 > " new
-    pkg install nodejs yarn build-essential python
+    $PACKAGE_CMD install nodejs yarn build-essential python
     yarn global add code-server
+    mkdir -p $PREFIX/var/service/code
+    cat <<- EOF >$PREFIX/var/service/code/run
+    #!/data/data/com.termux/files/usr/bin/sh
+    exec code-server 
+EOF
+    chmod +x $PREFIX/var/service/code/run
+    sv-enable code > /dev/null 2>&1
+}
+
+install_code_server2() {
+    echo -e "\n\n"
+    read -r -p "사용할 암호를 입력하세요 > " new
     config="$HOME/.config/code-server/config.yaml"
     old=`sed -n '3p' $config | awk '{print $2}'`
     sed -i "s/$old/$new/" $config           
-    mkdir -p $PREFIX/var/service/code
-    cat <<EOF >$PREFIX/var/service/code/run
-#!/data/data/com.termux/files/usr/bin/sh
-exec code-server
-EOF
-    chmod +x $PREFIX/var/service/code/run
-    sv-enable code
+    sv restart code > /dev/null 2>&1
+}
+
+stop_sjva() {
+    sv stop sjva > /dev/null 2>&1
+    ps -eo pid,args | grep start_termux_native | grep -v grep | awk '{print $1}' | xargs -r kill -9
+    ps -eo pid,args | grep sjva.py | grep -v grep | awk '{print $1}' | xargs -r kill -9
 }
 
 all() {
@@ -141,16 +152,21 @@ all() {
     install_rclone
     install_ffmpeg
     install_nginx
-    $PACKAGE_CMD -y install vim-python
+    $PACKAGE_CMD install vim-python
     pip3 install pillow wcwidth google-api-python-client guessit plexapi
+}
+
+install_aria() {
+    echo -e "\n\naria2 설치를 시작합니다."
+    $PACKAGE_CMD install aria2
+    echo -e "\n\nSJVA nginx 플러그인에서 웹 UI를 설치하세요."
 }
 
 while true; do
     menu
     read -n 1 -r -p "메뉴 선택 > " cmd
     case $cmd in
-        1) 
-            echo -e "\n\n권한을 허용해주세요."
+        1)  echo -e "\n\n권한을 허용해주세요."
             termux-setup-storage;;
         2) base;;
         3) install_filebrowser;;
@@ -158,10 +174,49 @@ while true; do
         5) install_ffmpeg;;
         6) install_nginx;;
         9) all;;
+        a)  echo -e "\n\naria2 설치를 시작합니다."
+            $PACKAGE_CMD install aria2
+            echo -e "\nSJVA nginx 플러그인에서 웹 UI를 설치하세요.";;
+        b)  echo -e "\n\nrclone 설치를 시작합니다."
+            $PACKAGE_CMD install rclone
+            version=`rclone --version`
+            echo -e "rclone 버전\n $version";;
+        c)  echo -e "\n\nsshd 설치를 시작합니다."
+            $PACKAGE_CMD install openssh
+            echo -e "\n암호를 입력하세요\n"
+            passwd
+            echo "IP : `ifconfig wlan0`"
+            echo "PORT : 8022"
+            echo -e "USER : $WHOAMI"
+            sv-enable sshd ;;
+        d)  echo -e "\n\ntransmission 설치를 시작합니다."
+            $PACKAGE_CMD install openssh
+            mv $PREFIX/share/transmission/web $PREFIX/share/transmission/web_default
+            git clone https://github.com/ronggang/transmission-web-control $HOME/twc
+            mv $HOME/twc/src $PREFIX/share/transmission/web
+            rm -rf $HOME/twc
+            echo -e "\n\nlocalhost:9091 or localhost:9999/transmission"
+            ;;
         e) install_code_server;;
+        f) install_code_server2;;
+        y) 
+            sv stop sshd > /dev/null 2>&1
+            sv stop nginx > /dev/null 2>&1
+            sv stop php-fpm > /dev/null 2>&1
+            sv stop code > /dev/null 2>&1
+            stop_sjva
+            ps -eo pid,args | grep sv | grep -v grep | awk '{print $1}' | xargs -r kill -9
+            sleep 1
+            ps -ef
+            echo -e "\n\nexit 명령을 입력하여 Termux를 종료하세요"
+            exit
+            ;;
+        z)
+            stop_sjva
+            $PREFIX/var/service/sjva/run
+            ;;
         *)
-            echo "AAA $cmd BBB"
-            #$PACKAGE_CMD clean
+            $PACKAGE_CMD clean
             echo -e "\n\n스크립트를 종료합니다.\n설치된 패키지가 있다면 Termux를 재시작하세요."
             exit 1
     esac
