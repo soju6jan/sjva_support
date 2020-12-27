ef7318522bac2758e3694bae511103ae9a46f62f
soju6jan <cybersol@naver.com>
2020-12-27 13:23:08 +0900

    1

--- termux/si.sh
+++ termux/si.sh
@@ -49,9 +49,11 @@ base() {
     $PACKAGE_CMD install termux-services git wget python libxml2 libxslt 
     python3 -m pip install --upgrade pip
     pip3 install --upgrade setuptools
-    pip3 install wheel
-    pip3 install flask-login flask-sqlalchemy pytz apscheduler selenium celery redis telepot sqlitedict lxml sqlalchemy gevent==20.9.0 gevent-websocket pycryptodome markdown psutil
-    pip3 install flask-socketio==4.3.1 python-engineio==3.13.2 python-socketio==4.6.0
+    pip3 install wheel==0.36.2
+    pip3 install gevent==20.9.0 lxml==4.6.2 Werkzeug==1.0.1 Jinja2==2.11.2 pycryptodome==3.9.9 SQLAlchemy==1.3.22   
+    pip3 install aiohttp==3.7.3 amqp==5.0.2 APScheduler==3.6.3 async-timeout==3.0.1 attrs==20.3.0 billiard==3.6.3.0 celery==5.0.5 chardet==3.0.4 click==7.1.2 click-didyoumean==0.0.3 click-plugins==1.1.1 click-repl==0.1.6 Flask==1.1.2 Flask-Login==0.5.0 Flask-SocketIO==4.3.1 Flask-SQLAlchemy==2.4.4 gevent-websocket==0.10.1 greenlet==0.4.17 idna==2.10 itsdangerous==1.1.0  kombu==5.0.2  Markdown==3.3.3 MarkupSafe==1.1.1 multidict==5.1.0 prompt-toolkit==3.0.8 psutil==5.8.0  python-engineio==3.13.2 python-socketio==4.6.0 pytz==2020.4 redis==3.5.3 selenium==3.141.0 six==1.15.0 sqlitedict==1.7.0 telepot==12.7 typing-extensions==3.7.4.3 tzlocal==2.1 urllib3==1.26.2 vine==5.0.0 wcwidth==0.2.5 yarl==1.6.3 zope.event==4.5.0 zope.interface==5.2.0
+    #pip3 install flask-login flask-sqlalchemy pytz apscheduler selenium celery redis telepot sqlitedict lxml sqlalchemy gevent==20.9.0 gevent-websocket pycryptodome markdown psutil
+    #pip3 install flask-socketio==4.3.1 python-engineio==3.13.2 python-socketio==4.6.0
 
     # sjva
     if [ -d $SJVA_HOME ]; then rm -rf $SJVA_HOME; fi
